f549795414ec9d4e2de6cd9c75a3ff1de567c0d3
Duke Jones <dukeofjones@gmail.com>
2018-12-08 23:27:21 -0800

    Fix typo. Add some flavor text.

--- install.fish
+++ install.fish
@@ -4,7 +4,10 @@ set -q XDG_CONFIG_HOME
     and set FISH_PATH "$XDG_CONFIG_HOME/fish"
     or set FISH_PATH "$HOME/.config/fish"
 
-git clone https://github.com/dukeones/triton.git $FISH_PATH/triton/github.com/dukejones/
+git clone https://github.com/dukejones/triton.git $FISH_PATH/triton/github.com/dukejones/
 mkdir -p $FISH_PATH/functions
 ln -s $FISH_PATH/triton/github.com/dukejones/triton/functions/triton.fish $FISH_PATH/functions/
 
+echo "Welcome to Triton!  You may with to run:
+echo "triton bootstrap"
+echo "To bootstrap a new level of fish configuration bliss.
